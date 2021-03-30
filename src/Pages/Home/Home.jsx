1cda9f545ca39c2fd42df382f34f5d4042dff105
sreeramvin <sreeramvin@gmail.com>
2021-03-30 10:20:31 +0530

    starting

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+
+const Landing = (props) => {
+   
+   
+    return (
+      <>
+        <div>
+            <h1>Heyyyyyy</h1>
+        </div>
+      </>
+    );
+  };
+  
+  export default Landing;
