b8aee9d5aba4f3465ed5a86f52ef097b140adcb9
rachel1306 <60882090+rachel1306@users.noreply.github.com>
2021-04-05 22:51:08 +0530

    Minor changes

--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -10,26 +10,46 @@ export default function Team() {
         <p className="staff-name">Sreenivas P</p>
         <p>STAFF IN CHARGE</p>
         </div>
+        <div className="core-team">
         <div className="flex-container">
             <div className="flex-item">
                 <img src={ core1 } className="core"/>
+                <p className="members">
+                    Name <br /> Designation 
+                </p>
             </div>
             <div className="flex-item">
                 <img src={ core1 } className="core"/>
+                <p className="members">
+                    Name <br /> Designation 
+                </p>
             </div>
             <div className="flex-item">
                 <img src={ core1 } className="core"/>
+                <p className="members">
+                    Name <br /> Designation 
+                </p>
             </div>
         </div>
         <div className="flex-container">
             <div className="flex-item">
                 <img src={ core1 } className="core"/>
+                <p className="members">
+                    Name <br /> Designation 
+                </p>
             </div>
             <div className="flex-item">
                 <img src={ core1 } className="core"/>
+                <p className="members">
+                    Name <br /> Designation 
+                </p>
             </div>
             <div className="flex-item">
                 <img src={ core1 } className="core"/>
+                <p className="members">
+                    Name <br /> Designation 
+                </p>
+            </div>
             </div>
         </div>
         </div>
