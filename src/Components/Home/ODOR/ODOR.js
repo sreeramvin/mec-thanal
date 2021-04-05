bb173ad2c7e51c17521fd262e8f43570eb28163b
Ashish <ashishmp0471@gmail.com>
2021-04-05 22:39:29 +0530

    Almost finished homepage

--- src/Components/Home/ODOR/ODOR.js
+++ src/Components/Home/ODOR/ODOR.js
@@ -1,7 +1,22 @@
+import './ODOR.css'
+import ODORImg from '../../../Assets/ODORImg.png'
 export default function OneDoneR(){
-    return(
+  return(
+      <div className="ODOR">
         <div>
-            OneDoneR
+          <p className="ODOR-heading">ONE DAY ONE RUPEE</p>
         </div>
-    )
+        <div className="ODOR-content">
+            <div className="ODOR-content-img">
+            <img src={ODORImg}/>
+          </div>
+          <div className="ODOR-content-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. At iaculis ipsum ut sem. Integer convallis nunc nec diam enim nullam nunc sed egestas. Vel hac et dolor aenean orci mattis. Vestibulum volutpat aenean at lacus tortor posuere. Eu, viverra vulputate ornare eu bibendum iaculis posuere quisque sapien.
+            </p>
+          </div>
+          
+        </div>
+      </div>
+  )
 }
