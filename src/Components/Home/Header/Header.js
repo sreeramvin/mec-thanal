7a6e28a50498a01f894400b7be414543747bcf2f
Ashish <ashishmp0471@gmail.com>
2021-04-08 11:22:56 +0530

    Fixed mobile view

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -2,16 +2,19 @@ import './Header.css'
 import LogoLarge from '../../../Assets/thanal.png'
 import frame from '../../../Assets/frame.png'
 export default function Header(){
-    return(
-        <div class="Header">
-            <div className="header-logo">
-                <img src={LogoLarge} className="header-logo-img" alt="pic"/>
-                <div className="header-logo-text">
-                    <p className="header-logo-text-p">THANAL MEC</p>
+    return( 
+        <div className="Header">
+            <div class="header-container">
+                <div className="header-logo">
+                    <img src={LogoLarge} className="header-logo-img" alt="pic"/>
+                    <div className="header-logo-text">
+                        <p className="header-logo-text-p">THANAL MEC</p>
+                    </div>
+                </div>
+                <div className="header-updates">
                 </div>
             </div>
-            <div className="header-updates">
-            </div>
+            <div className="header-updates-2"></div>
         </div>
     )
 }
