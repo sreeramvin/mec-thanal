ad0cbeb819692821a01db0acb343f852f01520d1
sreeramvin <sreeramvin@gmail.com>
2021-10-06 21:43:34 +0530

    changes made to deploy

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,23 +7,23 @@ import FPC from '../../Components/Home/FPC/FPC'
 import TS from '../../Components/Home/TS/TS'
 // import TopSection from '../../Components/Top-section/Top-section';
 import Header2 from '../../Components/Home/Header2/header2'
-import {useEffect, useRef, useState} from 'react'
+// import {useEffect, useRef, useState} from 'react'
 
 
 export default function Home(){
     // const aboutRef = useRef(null);
-    const [headerShown, setHeaderShown] = useState(false)
-    useEffect(() => {
-      window.addEventListener('scroll', scrollListener)
-      return () => window.removeEventListener('scroll', scrollListener)
-    },[])
+    // const [headerShown, setHeaderShown] = useState(false)
+    // useEffect(() => {
+    //   window.addEventListener('scroll', scrollListener)
+    //   return () => window.removeEventListener('scroll', scrollListener)
+    // },[])
 
-    const scrollListener = () => {
-      if(document.documentElement.scrollTop > window.innerHeight)
-        setHeaderShown(true)
-      else
-        setHeaderShown(false)
-    }
+    // const scrollListener = () => {
+    //   if(document.documentElement.scrollTop > window.innerHeight)
+    //     setHeaderShown(true)
+    //   else
+    //     setHeaderShown(false)
+    // }
     return (
       <div>
         {/* <TopSection headerShown = {true}/> */}
