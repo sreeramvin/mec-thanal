10dd6ca90cfe84efad95d6410a7a4fd0683e3566
sreeramvin <sreeramvin@gmail.com>
2021-10-11 12:13:15 +0530

    small changes in content

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -17,7 +17,7 @@ export default function About() {
       </div>
       <div className="about-content">
         <div className="about-content-text">
-          <p className="about-text" data-aos={"fade-down"}>
+          {/* <p className="about-text" data-aos={"fade-down"}>
             THANAL (To Humans A Noble Acme Life) is a non-profit
             organisation of  Govt. Model Engineering College, Cochin.
             THANAL was established as a students' initiative aiming to make a
@@ -30,6 +30,20 @@ export default function About() {
             expanded our resources to LinkedIn  in order to have a greater
             impact on the lives of those who are less fortunate.
             Keep an eye on this space for upcoming activities.
+          </p> */}
+          <p  className="about-text" data-aos={"fade-down"}>
+            To Humans, A Noble Acme Life (THANAL) is a social action club 
+            organized by the students of MEC dedicated to humanitarian welfare
+            and is responsible for raising funds and other amenities to help
+            the weaker sections of the society. We work on areas like child
+            and social empowerment and humanitarian welfare. THANAL aims
+            at encouraging and promoting among young professionals the 
+            drive to commit time from their busy schedules towards
+            fulfilling their obligations towards society
+            Thanal is a social action club dedicated to humanitarian welfare activities. We stand for:
+1. Creating awareness among students about the living conditions of our less fortunate fellow beings and to explore avenues where we can be of help to them.
+2. Cultivating the awareness that the dignity of the human beings is paramount, and is the source for all human rights.
+3. Spreading the message that charity is not a service, but a mandatory duty for every individual.
           </p>
         </div>
         {/* <div className="about-content-img">
--- src/Components/Home/ODOR/ODOR.js
+++ src/Components/Home/ODOR/ODOR.js
@@ -20,7 +20,15 @@ export default function OneDoneR(){
           </div>
           <div className="ODOR-content-text" data-aos={"fade-left"}>
             <p>
-            This is an initiative practised in MEC for long while, through which the students contribute one rupee every day and is collected through students representatives of THANAL from each class at the end of the month. This fund raised through this initiative acts as the major source of financial resource which is further used for the various charitable activities that THANAL organizes. This initiative kindles the spirit to give back to the society in each student and also nurtures the habit to look out for people in need of help. 
+            This is an initiative practised in MEC for long while,
+            through which the students contribute one rupee every day
+            and is collected through students representatives of THANAL
+            from each class at the end of the month. The fund raised 
+            through this initiative acts as the major source of financial
+            resource which is further used for the various charitable 
+            activities that THANAL organizes. This initiative kindles 
+            the spirit to give back to the society in each student and 
+            also nurtures the habit to look out for people in need of help. 
             </p>
           </div>
           
